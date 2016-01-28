ee4f46830cc6ba0a3949a82dd7f6c1bfce11dc1b
Pawan Kumar <Pawan.Kumar@ipsoft.com>
2016-01-28 19:21:25 +0530

    remove

--- src/main/java/net/org/myinfo/model/Role.java
+++
@@ -1,6 +0,0 @@
-package net.org.myinfo.model;
-
-public enum Role {
-	/*USER, ADMIN,SUPER,*/ROLE_USER, ROLE_ADMIN,ROLE_SUPER
-
-}
--- src/main/java/net/org/myinfo/service/OrgSubCategoryService.java
+++
@@ -1,9 +0,0 @@
-package net.org.myinfo.service;
-
-import net.org.myinfo.model.OrgSubCategory;
-
-public interface OrgSubCategoryService {
-    Iterable<OrgSubCategory> listAllOrgSubCategories();
-
-     
-}
